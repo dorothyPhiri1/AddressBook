eb9a4dea807353c8065e14834e02576f90fec633
Dorothy Phiri <dorothy.phiri@racunarstvo.hr>
2020-12-20 13:15:00 +0100

    Implemented the data layer of the application using repository and unit of work pattern.

---
+++ Extensions/ServiceExtentions.cs
@@ -0,0 +1,51 @@
+﻿using DataAccessLayer.DBInitialization;
+using DataAccessLayer.Repositories;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AddressBook.Extensions
+{
+    public static class ServiceExtentions
+    {
+        public static void ConfigureCors(this IServiceCollection services)
+        {
+            services.AddCors(options =>
+            {
+                options.AddPolicy("CorsPolicy",
+                    builder => builder.AllowAnyOrigin()
+                    .AllowAnyMethod()
+                    .AllowAnyHeader());
+            });
+        }
+
+        public static void ConfigureIISIntegration(this IServiceCollection services)
+        {
+            services.Configure<IISOptions>(options =>
+            {
+
+            });
+        }
+
+        //public static void ConfigureLoggerService(this IServiceCollection services)
+        //{
+        //    services.AddSingleton<ILoggerManager, LoggerManager>();
+        //}
+
+        public static void ConfigurePostgresqlContext(this IServiceCollection services, IConfiguration config)
+        {
+            var connectionString = config["postgresqlconnection:connectionString"];
+            services.AddDbContext<AddressBookDbContext>(o => o.UseNpgsql(connectionString));
+        }
+
+        public static void ConfigureUnitOfWork(this IServiceCollection services)
+        {
+            services.AddScoped<IUnitOfWork, AddressBookUnitOfWork>();
+        }
+    }
+}
--- Startup.cs
+++ Startup.cs
@@ -1,15 +1,11 @@
+using AddressBook.Extensions;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.HttpOverrides;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace AddressBook
 {
@@ -25,6 +21,12 @@ namespace AddressBook
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+
+            services.ConfigureCors();
+            services.ConfigureIISIntegration();
+            //services.ConfigureLoggerService();
+            services.ConfigurePostgresqlContext(Configuration);
+            services.ConfigureUnitOfWork();
 
             services.AddControllers();
             services.AddSwaggerGen(c =>
@@ -42,6 +44,17 @@ namespace AddressBook
                 app.UseSwagger();
                 app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "AddressBook v1"));
             }
+
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+
+            app.UseCors("CorsPolicy");
+
+            app.UseForwardedHeaders(new ForwardedHeadersOptions
+            {
+                ForwardedHeaders = ForwardedHeaders.All
+            });
+
 
             app.UseRouting();
 
